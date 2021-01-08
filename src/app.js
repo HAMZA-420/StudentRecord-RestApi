8db5673c75f8d51a41798b98303be2ec10488a54
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-01-08 12:26:05 +0500

    Adding data through Aysnc await

--- src/app.js
+++ src/app.js
@@ -11,17 +11,30 @@ app.get('/',(req,res)=> {
     res.send('Hello from the other side');
 })
 
-//create a new students
-app.post('/students', (req,res)=> {
+//create a new students with promises
+// app.post('/students', (req,res)=> {
     
-    console.log(req.body);  //getting data from postman
-    const user = new Student(req.body);
-    user.save().then(()=> {   //saving to mongodb
-        res.status(201).send(user);
-    }).catch((e)=> {
+//     console.log(req.body);  //getting data from postman
+//     const user = new Student(req.body);
+//     user.save().then(()=> {   //saving to mongodb
+//         res.status(201).send(user);
+//     }).catch((e)=> {
+//         res.status(400).send(e);
+//     }); 
+// })
+
+
+//create a new students with async await
+app.post("/students", async(req,res)=> {
+    try {
+        const user = new Student(req.body);
+        const createUser = await user.save();
+        res.status(201).send(createUser);
+    }catch(e) {
         res.status(400).send(e);
-    }); 
+    }
 })
+
 
 app.listen(port,()=>{
     console.log(`connection is setup at ${port}`);
